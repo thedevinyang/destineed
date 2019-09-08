1d794a521ce733915693c5230f1ad86a93d9d991
Devin Yang <thedevinyang@gmail.com>
2019-09-07 22:11:53 -0700

    send_sms now reads phonenumbers directly from the numbers.txt file.

--- routes/send_sms.js
+++ routes/send_sms.js
@@ -5,10 +5,18 @@ const accountSid = 'AC7b52c9e288a9a128abe2da90a5026cac';
 const authToken = '4636d2429edae909298d79888f9489b0';
 const client = require('twilio')(accountSid, authToken);
 
-client.messages
-  .create({
-     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-     from: '+18313464554',
-     to: '+14089300543'
-   })
-  .then(message => console.log(message.sid));
+var fs = require('fs');
+
+fs.readFile('numbers.txt', 'utf8', function(err, data) {
+    if (err) throw err;
+    console.log(data);
+    var phoneNumber = "+1" + data;
+
+    client.messages
+      .create({
+         body: 'Hi! You are now recieving text alerts from Destineed!',
+         from: '+18313464554',
+         to: phoneNumber
+       })
+      .then(message => console.log(message.sid));
+});
