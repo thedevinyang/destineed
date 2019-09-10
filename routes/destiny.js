da53407e9361b857f36f65fd3b003740990678ed
Devin Yang <thedevinyang@gmail.com>
2019-09-09 19:38:57 -0700

    Added getVendors function

--- routes/destiny.js
+++ routes/destiny.js
@@ -25,10 +25,27 @@ request.post({
     console.log(body);
     access_token = body.access_token;
     //console.log(body + " \nstatus code: " + resp.statusCode + "\ncookie: " + resp.headers['set-cookie']);
+    getVendors();
     res.redirect("/");
   });
 }
 
 exports.getVendors = function(req, res) {
+  const charID = 2305843009299856596;
+  const memID = 4611686018470735882;
+  const memType = 4;
+  const vendorHash = 1265988377;
 
+request.get({
+  url: 'https://www.bungie.net/Platform/Destiny2/' + memType + '/Profile/' + memID + '/Character/' + charID + '/Vendors/' + vendorHash + '?components=402',
+  headers: {
+    'X-API-Key': "f966c6ece9324106b01d501a6d932ff6",
+    'Authorization': 'Bearer ' + access_token,
+    'Host': 'www.bungie.net',
+    'Connection': 'keep-alive'
+  },
+  }, function(err, resp, body){
+    console.log('Printing vendor body.' + body);
+
+  });
 }
