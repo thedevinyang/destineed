2501aecf7a0798f4bb3ad54bd49a4411d911fb62
Devin Yang <thedevinyang@gmail.com>
2019-09-09 19:50:56 -0700

    Changed the way we are trying to access body.accesstoken

--- routes/destiny.js
+++ routes/destiny.js
@@ -23,7 +23,7 @@ request.post({
     }
   }, function(err, resp, body) {
     console.log(body);
-    access_token = body.access_token;
+    access_token = body['access_token'];
     console.log(access_token);
     //console.log(body + " \nstatus code: " + resp.statusCode + "\ncookie: " + resp.headers['set-cookie']);
     getVendors();
