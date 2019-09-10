3cde88d0cecd75c8ca5d00b452a723550712277f
Michelle Yang <zomgitsmichelle@gmail.com>
2019-09-09 17:23:47 -0700

    Testing removing express.

--- routes/destiny.js
+++ routes/destiny.js
@@ -19,20 +19,20 @@ exports.getAccessToken = function(req, res){
   const client_id = "29743";
   const client_secret = "1KQkDDBAX4Fg7R4MonEuikYz9LQWDTLNuKuDKXXSQfs"; //TODO
   const code = res.get("code"); // TODO
-
-  request.post({
-    url: "https://www.bungie.net/platform/app/oauth/token/?grant_type=authorization_code&client_id=29743&code=142a4e57bd3c6f8451cd46cf4574a944",
-    header: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      "X-API-Key": "f966c6ece9324106b01d501a6d932ff6",
-      "Authorization": "Basic " + base64encode(client_id+":"+client_secret)
-    },
-    data: {
-      "grant_type": "authorization_code",
-      "client_id": client_id,
-      "code": code
-    }
-  }, function(err, res, body){
-    console.log(body);
-  });
+  //
+  // request.post({
+  //   url: "https://www.bungie.net/platform/app/oauth/token/?grant_type=authorization_code&client_id=29743&code=142a4e57bd3c6f8451cd46cf4574a944",
+  //   header: {
+  //     "Content-Type": "application/x-www-form-urlencoded",
+  //     "X-API-Key": "f966c6ece9324106b01d501a6d932ff6",
+  //     "Authorization": "Basic " + base64encode(client_id+":"+client_secret)
+  //   },
+  //   data: {
+  //     "grant_type": "authorization_code",
+  //     "client_id": client_id,
+  //     "code": code
+  //   }
+  // }, function(err, res, body){
+  //   console.log(body);
+  // });
 }
